1bbdada21ac8afc50e872232e41de84b47b13505
Andrew Iasko <andrii.iasko@gmail.com>
2016-10-01 01:41:29 +0300

    Setup api project

--- src/ShortestWayFinder.Web/Startup.cs
+++ src/ShortestWayFinder.Web/Startup.cs
@@ -1,37 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
+using ShortestWayFinder.Domain;
+using ShortestWayFinder.Domain.Infrastructure.Contracts;
 
 namespace ShortestWayFinder.Web
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+        
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<DataDbContext>(opt => opt.UseInMemoryDatabase());
+
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        public void Configure(IApplicationBuilder app, IDatabaseInitializer databaseInitializer)
         {
-            loggerFactory.AddConsole();
+            app.UseStaticFiles();
+            app.UseCors(builder =>
+                // This will allow any request from any server. 
+                builder
+                .AllowAnyHeader()
+                .AllowAnyMethod()
+                .AllowAnyOrigin());
 
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            // Add MVC to the request pipeline.
+            app.UseDeveloperExceptionPage();
+            app.UseMvc();
         }
     }
 }
