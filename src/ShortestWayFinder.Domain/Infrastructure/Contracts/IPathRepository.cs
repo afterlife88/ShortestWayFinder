cfb729c20576d62c3080cf9943b8ea118405f837
Andrew Iasko <andrii.iasko@gmail.com>
2016-10-01 02:24:33 +0300

    Completed basic operatiopn on path repo

--- src/ShortestWayFinder.Domain/Infrastructure/Contracts/IPathRepository.cs
+++ src/ShortestWayFinder.Domain/Infrastructure/Contracts/IPathRepository.cs
@@ -1,7 +1,14 @@
-﻿namespace ShortestWayFinder.Domain.Infrastructure.Contracts
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using ShortestWayFinder.Domain.DatabaseModels;
+
+namespace ShortestWayFinder.Domain.Infrastructure.Contracts
 {
     public interface IPathRepository
     {
-        
+        Task<IEnumerable<Path>> GetAllAsync();
+        Task<Path> GetByPointsNames(string firstPointName, string secondPointName);
+        Task<Path> Get(int id);
+        Task<int> AddPath(Path model);
     }
 }
--- src/ShortestWayFinder.Domain/Infrastructure/Repositories/PathRepository.cs
+++ src/ShortestWayFinder.Domain/Infrastructure/Repositories/PathRepository.cs
@@ -1,8 +1,44 @@
-﻿using ShortestWayFinder.Domain.Infrastructure.Contracts;
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ShortestWayFinder.Domain.DatabaseModels;
+using ShortestWayFinder.Domain.Infrastructure.Contracts;
 
 namespace ShortestWayFinder.Domain.Infrastructure.Repositories
 {
     public class PathRepository : IPathRepository
     {
+        private readonly DataDbContext _dataDbContext;
+
+        public PathRepository(DataDbContext dataDbContext)
+        {
+            _dataDbContext = dataDbContext;
+        }
+
+        public async Task<int> AddPath(Path model)
+        {
+            _dataDbContext.Paths.Add(model);
+            return await _dataDbContext.SaveChangesAsync();
+        }
+        public async Task<IEnumerable<Path>> GetAllAsync()
+        {
+            return await _dataDbContext.Paths.ToArrayAsync();
+        }
+
+        public async Task<Path> Get(int id)
+        {
+            return await _dataDbContext.Paths.SingleOrDefaultAsync(r => r.Id == id);
+        }
+
+        public async Task<Path> GetByPointsNames(string firstPointName, string secondPointName)
+        {
+            return
+                await
+                    _dataDbContext.Paths.FirstOrDefaultAsync(
+                        r =>
+                            string.Equals(r.FirstPoint, firstPointName, StringComparison.CurrentCultureIgnoreCase) &&
+                            string.Equals(r.SecondPoint, secondPointName, StringComparison.CurrentCultureIgnoreCase));
+        }
     }
 }
