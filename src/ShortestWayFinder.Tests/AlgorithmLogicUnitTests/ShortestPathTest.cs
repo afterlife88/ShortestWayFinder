f4088251d173ddad1c0206a5a7782ef25a2fed30
Andrew Iasko <andrii.iasko@gmail.com>
2016-10-01 01:04:05 +0300

    Added 2 test on logic

--- src/ShortestWayFinder.Tests/AlgorithmLogicUnitTests/ShortestPathTest.cs
+++ src/ShortestWayFinder.Tests/AlgorithmLogicUnitTests/ShortestPathTest.cs
@@ -13,7 +13,7 @@ namespace ShortestWayFinder.Tests.AlgorithmLogicUnitTests
         public void GetShortestPath_ScenarioATest()
         {
             //Arrange
-            IEnumerable<Edge> edges = new[]
+            var edges = new[]
             {
                 new Edge{Start="o",End="a",Cost=2},
                 new Edge{Start="o",End="b",Cost=5},
@@ -41,14 +41,49 @@ namespace ShortestWayFinder.Tests.AlgorithmLogicUnitTests
             //Act
             IList<List<Edge>> path = targt.GetShortestPath("o", "t");
 
+            // Expecting shortest path: o=>a=>b=>d=>t
+            // You can view graphic result:
+            // https://s16.postimg.org/jv5lzfxr9/scenario_A.png
+
             //Assert
-            Assert.Equal(2, path.Count);
-
+            // 4 edges are in short path
             Assert.Equal(4, path[0].Count);
+            // Sum of cost (time) on edges to shortest path 
+            // 2+2+4+5=13
             Assert.Equal(13, path[0].Sum(e => e.Cost));
-
+            // 5 Vertex are contains on short path
             Assert.Equal(5, path[1].Count);
             Assert.Equal(13, path[1].Sum(e => e.Cost));
+        }
+
+        [Fact]
+        public void GetShortestPath_ScenarioBTest()
+        {
+            //Arrange
+            var edges = new[]
+            {
+                new Edge{Start="a",End="b",Cost=1},
+                new Edge{Start="b",End="c",Cost=1000},
+                new Edge{Start="c",End="d",Cost=1},
+                new Edge{Start="d",End="a",Cost=100},
+            };
+            IShortestPath targt = new ShortestPath(edges);
+
+            //Act
+            IList<List<Edge>> path = targt.GetShortestPath("a", "c");
+
+            // Expecting shortest path: a=>d=>c
+            // You can view graphic result:
+            // https://s12.postimg.org/qpcibaoy5/scenario_B.png
+            //Assert
+            Assert.Equal(1, path.Count);
+
+            // 2 edges are in short path
+            Assert.Equal(2, path[0].Count);
+
+            // Sum of cost (time) on edges to shortest path 
+            // 100 + 1
+            Assert.Equal(101, path[0].Sum(e => e.Cost));
         }
     }
 }
