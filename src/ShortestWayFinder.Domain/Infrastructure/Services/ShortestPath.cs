5871f9f832859d36891b0c5a33aad09bf1db2574
Andrew Iasko <andrii.iasko@gmail.com>
2016-10-01 00:11:53 +0300

    Added tests to alghoritm

--- src/ShortestWayFinder.Domain/Infrastructure/Services/ShortestPath.cs
+++ src/ShortestWayFinder.Domain/Infrastructure/Services/ShortestPath.cs
@@ -1,11 +1,205 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
+using ShortestWayFinder.Domain.Entities;
+using ShortestWayFinder.Domain.Infrastructure.Contracts;
 
 namespace ShortestWayFinder.Domain.Infrastructure.Services
 {
-    public class ShortestPath
+    public class ShortestPath : IShortestPath
     {
+        #region Member Variables
+
+        Dictionary<string, Node> _matrix;
+        HashSet<Node> _visitedNodes;
+
+        #endregion
+
+        #region Constructor
+
+        public ShortestPath(IEnumerable<Edge> graph)
+        {
+            _matrix = CreateMatrix(graph);
+            _visitedNodes = new HashSet<Node>();
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        IList<List<Edge>> IShortestPath.GetShortestPath(string source, string destination)
+        {
+            _matrix[source].CostFromSource = 0;
+            _visitedNodes.Add(_matrix[source]);
+            Node destinationNode = _matrix[destination];
+            Node minimumNode = new Node();
+            var edges = new List<List<Edge>>();
+
+            while (!_visitedNodes.Contains(destinationNode))
+            {
+                minimumNode = GetMinimumNode();
+
+                if (minimumNode.Label == null)//No Path exists
+                {
+                    return edges;
+                }
+
+                _visitedNodes.Add(minimumNode);
+            }
+
+            edges.Add(new List<Edge>());
+            GetPaths(source, minimumNode, edges, edges.Count - 1);
+
+            return edges;
+        }
+
+        #endregion
+
+        #region Public Properties
+
+        Dictionary<string, Node> IShortestPath.Matrix
+        {
+            get
+            {
+                return _matrix;
+            }
+            set
+            {
+                _matrix = value; ;
+            }
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        private Dictionary<string, Node> CreateMatrix(IEnumerable<Edge> graph)
+        {
+            var matrix = new Dictionary<string, Node>();
+
+            foreach (var edge in graph)
+            {
+                Node firstVertex;
+
+                if (!matrix.ContainsKey(edge.Start))
+                {
+                    firstVertex = new Node { Label = edge.Start, Neighbors = new Dictionary<string, int>() };
+                    matrix.Add(edge.Start, firstVertex);
+                }
+
+                firstVertex = matrix[edge.Start];
+
+                Node secondVertex;
+
+                if (!matrix.ContainsKey(edge.End))
+                {
+                    secondVertex = new Node { Label = edge.End, Neighbors = new Dictionary<string, int>() };
+                    matrix.Add(edge.End, secondVertex);
+                }
+
+                secondVertex = matrix[edge.End];
+
+                if (!firstVertex.Neighbors.ContainsKey(secondVertex.Label))
+                {
+                    firstVertex.Neighbors.Add(secondVertex.Label, edge.Cost);
+                }
+                if (!secondVertex.Neighbors.ContainsKey(firstVertex.Label))
+                {
+                    secondVertex.Neighbors.Add(firstVertex.Label, edge.Cost);
+                }
+            }
+
+            return matrix;
+        }
+
+        private Node GetMinimumNode()
+        {
+            var minimumNodeSoFar = new Node();
+
+            foreach (Node currentNode in _visitedNodes)
+            {
+                IEnumerable<Node> unVisitedNeighbors = GetUnVisitedNeighbors(currentNode);
+
+                if (unVisitedNeighbors.Any())
+                {
+                    RelaxNeighboringNodes(currentNode, unVisitedNeighbors);
+
+                    Node minimumNodeEndingHere = unVisitedNeighbors.Aggregate((a, b) => a.CostFromSource < b.CostFromSource ? a : b);
+
+                    if (!minimumNodeSoFar.CostFromSource.HasValue || minimumNodeSoFar.CostFromSource > minimumNodeEndingHere.CostFromSource)
+                    {
+                        minimumNodeSoFar = minimumNodeEndingHere;
+                    }
+                }
+            }
+
+            return minimumNodeSoFar;
+        }
+
+        private IEnumerable<Node> GetUnVisitedNeighbors(Node currentNode)
+        {
+            IEnumerable<Node> unVisitedNeighbors = currentNode.Neighbors
+                .Where(n => !_visitedNodes.Contains(new Node { Label = n.Key }))
+                .Select(n => _matrix[n.Key]);
+            return unVisitedNeighbors;
+        }
+
+        private void RelaxNeighboringNodes(Node currentNode, IEnumerable<Node> unVisitedNeighbors)
+        {
+            foreach (Node neighbor in unVisitedNeighbors)
+            {
+                int? newCost = currentNode.CostFromSource + currentNode.Neighbors[neighbor.Label];
+
+                if (neighbor.CostFromSource.HasValue)
+                {
+                    if (newCost < neighbor.CostFromSource)
+                    {
+                        neighbor.CostFromSource = newCost;
+                        neighbor.Parents = new HashSet<Node> { currentNode };
+                    }
+                    else if (newCost == neighbor.CostFromSource && !neighbor.Parents.Contains(currentNode))//in case of tie
+                    {
+                        neighbor.Parents.Add(currentNode);
+                    }
+                }
+                else
+                {
+                    neighbor.CostFromSource = newCost;
+                    neighbor.Parents = new HashSet<Node> { currentNode };
+                }
+            }
+        }
+
+        private void GetPaths(string source, Node destination, List<List<Edge>> edges, int level)
+        {
+            if (destination.Parents == null)
+            {
+                return;
+            }
+
+            int i = 0;
+            List<Edge> routeCopy = new List<Edge>(edges[level]);//make a copy of the current route
+
+            foreach (Node originNode in destination.Parents)
+            {
+                if (i > 0)//only branch after first edge, as first edge is added to default route
+                {
+                    edges.Add(routeCopy);
+                    level++;
+                }
+
+                var edge = new Edge
+                {
+                    Start = originNode.Label,
+                    End = destination.Label,
+                    Cost = _matrix[originNode.Label].Neighbors[destination.Label]
+                };
+
+                edges[level].Add(edge);
+                GetPaths(source, originNode, edges, level);
+                i++;
+            }
+        }
+
+        #endregion
     }
 }
---
+++ src/ShortestWayFinder.Tests/AlgorithmLogicUnitTests/ShortestPathTest.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using ShortestWayFinder.Domain.Entities;
+using ShortestWayFinder.Domain.Infrastructure.Contracts;
+using ShortestWayFinder.Domain.Infrastructure.Services;
+using Xunit;
+
+namespace ShortestWayFinder.Tests.AlgorithmLogicUnitTests
+{
+    public class ShortestPathTest
+    {
+        [Fact]
+        public void GetShortestPath_ScenarioATest()
+        {
+            //Arrange
+            IEnumerable<Edge> edges = new[]
+            {
+                new Edge{Start="o",End="a",Cost=2},
+                new Edge{Start="o",End="b",Cost=5},
+                new Edge{Start="o",End="c",Cost=4},
+
+                new Edge{Start="a",End="f",Cost=12},
+                new Edge{Start="a",End="d",Cost=7},
+                new Edge{Start="a",End="b",Cost=2},
+
+                new Edge{Start="b",End="d",Cost=4},
+                new Edge{Start="b",End="e",Cost=3},
+                new Edge{Start="b",End="c",Cost=1},
+
+                new Edge{Start="c",End="e",Cost=4},
+
+                new Edge{Start="d",End="e",Cost=1},
+                new Edge{Start="d",End="t",Cost=5},
+
+                new Edge{Start="e",End="t",Cost=7},
+
+                new Edge{Start="f",End="t",Cost=3}
+            };
+            IShortestPath targt = new ShortestPath(edges);
+
+            //Act
+            IList<List<Edge>> path = targt.GetShortestPath("o", "t");
+
+            //Assert
+            Assert.Equal(2, path.Count);
+
+            Assert.Equal(4, path[0].Count);
+            Assert.Equal(13, path[0].Sum(e => e.Cost));
+
+            Assert.Equal(5, path[1].Count);
+            Assert.Equal(13, path[1].Sum(e => e.Cost));
+        }
+    }
+}
