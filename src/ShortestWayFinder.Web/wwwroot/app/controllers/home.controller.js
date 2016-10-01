e146ec1b86ff2b670d61cc48fe05a463eacc0bf2
Andrew Iasko <andrii.iasko@gmail.com>
2016-10-02 02:40:06 +0300

    Remove path completed

--- src/ShortestWayFinder.Web/wwwroot/app/controllers/home.controller.js
+++ src/ShortestWayFinder.Web/wwwroot/app/controllers/home.controller.js
@@ -12,6 +12,8 @@
     vm.allPaths = [];
     vm.shortestPathData = {};
     vm.getShortestPath = getShortestPath;
+    vm.editPath = editPath;
+    vm.removePath = removePath;
 
     getPoints();
     getPaths();
@@ -65,6 +67,32 @@
     function getShortestPath(data) {
       return PathService.getShortestPath(data).then(function (response) {
         console.log(response);
+      }).catch(function (err) {
+        console.log(err);
+        //switch (err.status) {
+        //  case 400:
+        //    vm.errorMsg = 'Some values are invalid!';
+        //    break;
+        //  case 401:
+        //    vm.errorMsg = 'Auth token are missing!';
+        //    break;
+        //  case 500:
+        //    vm.erorMsg = err.data;
+        //    break;
+        //  default:
+        //    vm.errorMsg = 'Something wrong...';
+        //    break;
+        //}
+      });
+    }
+    function editPath(data, id) {
+      console.log(data, id);
+    }
+    function removePath(id, item) {
+      return PathService.removePath(id).then(function (response) {
+        var index = vm.allPaths.indexOf(item);
+        vm.allPaths.splice(index, 1);
+        console.log(vm.allPaths);
       }).catch(function (err) {
         console.log(err);
         //switch (err.status) {
--- src/ShortestWayFinder.Web/wwwroot/app/services/pathService.js
+++ src/ShortestWayFinder.Web/wwwroot/app/services/pathService.js
@@ -10,27 +10,26 @@
     var pathService = {
       getPoints: getPoints,
       getShortestPath: getShortestPath,
-      getAllPaths: getAllPaths
+      getAllPaths: getAllPaths,
+      removePath: removePath
     };
 
     return pathService;
 
     function getPoints() {
       spinnerService.showSpinner();
-      return $http.get('api/path/points')
-        .then(function (response) {
-          spinnerService.hideSpinner();
-          return response.data;
-        }).catch(function (data) {
-          spinnerService.hideSpinner();
-          return $q.reject(data);
-        });
+      return $http.get('api/path/points').then(function (response) {
+        spinnerService.hideSpinner();
+        return response.data;
+      }).catch(function (data) {
+        spinnerService.hideSpinner();
+        return $q.reject(data);
+      });
     }
 
     function getShortestPath(data) {
       spinnerService.showSpinner();
-      return $http.post('api/path/shortestpath', data)
-      .then(function (response) {
+      return $http.post('api/path/shortestpath', data).then(function (response) {
         spinnerService.hideSpinner();
         return response.data;
       }).catch(function (data) {
@@ -41,8 +40,18 @@
 
     function getAllPaths() {
       spinnerService.showSpinner();
-      return $http.get('api/path')
-      .then(function (response) {
+      return $http.get('api/path').then(function (response) {
+        spinnerService.hideSpinner();
+        return response.data;
+      }).catch(function (data) {
+        spinnerService.hideSpinner();
+        return $q.reject(data);
+      });
+    }
+
+    function removePath(id) {
+      spinnerService.showSpinner();
+      return $http.delete('api/path/' + id).then(function (response) {
         spinnerService.hideSpinner();
         return response.data;
       }).catch(function (data) {
@@ -62,30 +71,5 @@
     //    return $q.reject(data);
     //  });
     //}
-    //function getUserByEmail(email, token) {
-    //  spinner.showWait();
-    //  return $http.get('api/User/getUser/' + email,
-    //  {
-    //    headers: { 'Authorization': token }
-    //  }).then(function (response) {
-    //    spinner.hideWait();
-    //    return response;
-    //  }).catch(function (data) {
-    //    spinner.hideWait();
-    //    return $q.reject(data);
-    //  });
-    //}
-    //function getUserById(userId) {
-    //  spinner.showWait();
-    //  return $http.get('api/User/' + userId
-    //  ).then(function (response) {
-    //    spinner.hideWait();
-    //    return response;
-    //  }).catch(function (data) {
-    //    spinner.hideWait();
-    //    return $q.reject(data);
-    //  });
-    //}
-
   }
 })();
